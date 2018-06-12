29989011d9c295d00aa4d3417d185011360dd357
Alex Zhang <alexgzhang92@gmail.com>
2018-06-12 20:19:32 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,9 +4,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books INNER JOIN series ON books.series_id = series.id WHERE series.id = 1 ORDER BY books.year"
 end
 
-it 'returns the name and motto of the character with the longest motto' do
-    expect(@db.execute(select_name_and_motto_of_char_with_longest_motto)).to eq([["Tyrion Lannister", "A Lannister always pays his debts"]])
-  end
+
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters WHERE characters.motto = (SELECT MAX(characters.motto) FROM characters)"
