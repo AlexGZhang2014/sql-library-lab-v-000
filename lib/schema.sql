8257480ffe508b78e5d4b90080e2b641b25c50e0
Alex Zhang <alexgzhang92@gmail.com>
2018-06-12 19:50:16 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,12 +4,3 @@ CREATE TABLE series (
   author_id INTEGER,
   subgenre_id INTEGER
 );
-
-    it "creates a series table with a subgenre_id field" do
-      expect{@db.execute("SELECT subgenre_id FROM series;")}.to_not raise_exception
-    end
-
-    it "creates a series table with an id as a primary key" do
-      expect(@db.execute("PRAGMA table_info(series);").detect { |arr| arr[-1] == 1 && arr[2] == "INTEGER" }.length).to eq(6)
-    end
-  end
