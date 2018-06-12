fb2ad619f2231a254ccdbc29c48abbca12f341ff
Alex Zhang <alexgzhang92@gmail.com>
2018-06-12 21:51:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
    INNER JOIN characters ON series.id = characters.series_id
    GROUP BY series.title
-   ORDER BY COUNT(character.species) DESC;"
+   ORDER BY COUNT(character.species) DESC
+   LIMIT(1);"
 end
 
 #it 'selects all of the character names and their number of books they have appeared in, in descending order' do
