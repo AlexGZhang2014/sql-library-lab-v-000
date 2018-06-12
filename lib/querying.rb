712cc55be606efaafa819f4500188e3e2b2cb593
Alex Zhang <alexgzhang92@gmail.com>
2018-06-12 21:45:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = \'human\' LIMIT(1);"
+  "SELECT series.title FROM series
+   INNER JOIN characters ON series.id = characters.series_id
+   WHERE characters.species = \'human\' LIMIT(1);"
 end
 
 #it 'selects all of the character names and their number of books they have appeared in, in descending order' do
