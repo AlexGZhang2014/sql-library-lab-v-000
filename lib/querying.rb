2eb48feb2938170628406ac0de1fe38bd5b884a5
Alex Zhang <alexgzhang92@gmail.com>
2018-06-12 20:11:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ it 'selects all of the books titles and years in the first series and orders the
   end
 
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  ""
 end
 
 def select_name_and_motto_of_char_with_longest_motto
