82f2abf25626b7112cafacde17b1d2bb3cd439c5
Alex Zhang <alexgzhang92@gmail.com>
2018-06-12 21:44:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,8 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) FROM characters WHERE characters.species = (SELECT MAX(characters.species) FROM characters);"
+  "SELECT characters.species, COUNT(characters.species) FROM characters
+   WHERE characters.species = (SELECT MAX(characters.species) FROM characters);"
 end
 
 def select_name_and_series_subgenres_of_authors
