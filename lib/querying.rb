2b068dca649a06669890db10d0e22deca1bf71c2
Alex Zhang <alexgzhang92@gmail.com>
2018-06-12 21:19:16 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,6 +2,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books INNER JOIN series ON books.series_id = series.id WHERE series.id = 1 ORDER BY books.year;"
 end
 
+it 'returns the name and motto of the character with the longest motto' do
+    expect(@db.execute(select_name_and_motto_of_char_with_longest_motto)).to eq([["Tyrion Lannister", "A Lannister always pays his debts"]])
+  end
 
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -9,6 +12,9 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 
+  it 'determines the most prolific species of characters and return its value and count' do
+    expect(@db.execute(select_value_and_count_of_most_prolific_species)).to eq([["human", 4]])
+  end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, SUM(characters.species) FROM characters WHERE characters.species = (SELECT MAX(character.species) FROM characters);"
